a12a2dba8804880d2b4a057d821b325e2655f127
AndreNIH <31828626+AndreNIH@users.noreply.github.com>
2022-10-09 20:47:42 -0700

    Move DLLMain to dllmain.cpp

--- src/CustomProfileBadges.cpp
+++ src/CustomProfileBadges.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "Hooks/ProfilePage.hpp"
 #include "Hooks/CommentCell.hpp"
 #include "CustomProfileBadges.hpp"
@@ -89,31 +88,3 @@ CustomBadgeManager::CustomBadgeManager(){
     CommentCell::hookInit();
 }
 
-
-
-
-//Boilerplate code
-/*DWORD WINAPI modThread(void* hModule){
-    AllocConsole();
-    freopen_s(reinterpret_cast<FILE**>(stdout), "CONOUT$", "w", stdout);
-    freopen_s(reinterpret_cast<FILE**>(stdin), "CONIN$", "r", stdin);
-    spdlog::set_level(spdlog::level::trace);
-    ProfilePage::hookInit();
-    CommentCell::hookInit();
-    return 0;
-}*/
-
-
-BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call,LPVOID lpReserved)
-{
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-        //CreateThread(0, 0, modThread, hModule, 0, 0);
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
-}
